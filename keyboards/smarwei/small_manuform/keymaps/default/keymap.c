9f55432a50488c2724be53ccfd41c2cdae44d33e
Arne Weiss <arne.weiss@udo.edu>
2022-12-03 21:20:29 +0100

    .

--- keyboards/smarwei/small_manuform/keymaps/default/keymap.c
+++ keyboards/smarwei/small_manuform/keymaps/default/keymap.c
@@ -1,36 +1,36 @@
 #include QMK_KEYBOARD_H
 
-// TD(0) = On Tap -> MO(2); On hold -> MO(2); On double tap -> TO(3)
+// Tap Dance declarations
+enum {
+    LAYR,
+};
 
-bool caps_word_press_user(uint16_t keycode) {
-    switch (keycode) {
-        // Keycodes that continue Caps Word, with shift applied.
-        case KC_A:
-            return true;
-        case KC_B ... KC_Z:
-        case TD(4):
-        case TD(5):
-        case TD(6):
-        case TD(7):
-        case KC_MINS:
-            add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
-            return true;
-
-        // Keycodes that continue Caps Word, without shifting.
-        case KC_1 ... KC_0:
-        case KC_BSPC:
-        case KC_DEL:
-        case KC_UNDS:
-            return true;
-
-        default:
-            return false;  // Deactivate Caps Word.
-    }
-}
-
+//  Tap Dance definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+    // TD(0) = On Tap -> MO(2); On hold -> MO(2); On double tap -> TO(3)
+    [LAYR] = ACTION_TAP_DANCE_DOUBLE(MO(2), TO(0)),
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
+    /*
+     * ┌───┬───┬───┬───┬───┬───┐
+     * │F7 │F8 │F9 │F10│F11|F12|
+     * ├───┼───┼───┼───┼───┼───┤
+     * │ 6 │ 7 │ 8 │ 9 │ 0 |TG |
+     * ├───┼───┼───┼───┼───┼───|
+     * │ J │ L │ U │ Y │ ; |F24|
+     * ├───┼───┼───┼───┼───┼───|
+     * │ M │ N │ E │ I │ O |CPS|
+     * ├───┼───┼───┼───┼───┼───|
+     * │ K │ H │ , │ . │ / |PRT|
+     * └───┴───┼───┼───┼───┴───┘
+     *         │ - │ = |
+     * ┌───┬───┼───┼───┘
+     * │RET│SPC│MO1│
+     * ├───┼───┼───┘
+     * │LUP│LDN│
+     * └───┴───┘
+     */
     [0] = LAYOUT_std(
         KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,    KC_F6,       KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,
         KC_TILD, KC_1,    KC_2,     KC_3,    KC_4,     KC_5,        KC_6,    KC_7,    KC_8,    KC_9,     KC_0,     SH_TG,
@@ -39,8 +39,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         CAPSWRD, KC_Z,    KC_X,     KC_C,    KC_D,     KC_V,        KC_K,    KC_H,    KC_COMM, KC_DOT,   KC_SLASH, KC_RSFT,
                           KC_LGUI,  KC_LALT,                                          KC_MINS, KC_EQL,
 
-                                  KC_LCTL, MO(1),    KC_LSFT,       KC_ENT,  MO(2),   KC_SPC,
-                                           KC_LCTL,  KC_LALT,       KC_LGUI, TT(3) 
+                                  KC_LCTL, MO(1),    KC_LSFT,       KC_ENT,  TD(0),   KC_SPC,
+                                           KC_LCTL,  KC_LALT,       KC_LGUI, KC_LSFT
 
     ),
 
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______, _______,
       _______, KC_ESC,  KC_WH_U, KC_WBAK, KC_WFWD, KC_MS_U,        KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_DEL,  KC_ESC,
       _______, KC_LALT, KC_WH_D, KC_LSFT, KC_LCTL, KC_MS_D,        KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC, KC_MENU,
-      _______, KC_WH_L, KC_UNDO, KC_CUT,  KC_COPY, C(S(KC_V)),     KC_BTN1, KC_BTN2, KC_BTN3, KC_MS_L, KC_MS_R, KC_WH_R,
+      _______, KC_WH_L, KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE,        KC_BTN1, KC_BTN2, KC_BTN3, KC_MS_L, KC_MS_R, KC_WH_R,
                         _______, _______,                                            KC_0,    KC_INS,
 
                                  _______, _______,  _______,       _______, _______, _______,
@@ -60,9 +60,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [2] = LAYOUT_std(
        _______,    _______,    _______,    _______,    _______,   _______,       _______,    _______,    _______,    _______,    _______,   _______,
        _______,    _______,    _______,    _______,    _______,   _______,       _______,    _______,    _______,    _______,    _______,   _______,
-       _______,    _______,    S(KC_MINS), S(KC_LBRC), S(KC_RBRC),S(KC_6),       S(KC_1),    S(KC_COMM), S(KC_DOT),  KC_EQL,     S(KC_7),   _______,
-       _______,    KC_NUBS,    KC_SLSH,    KC_LBRC,    KC_RBRC,   S(8),          S(KC_SLSH), S(KC_9),    S(KC_0),    KC_MINS,    S(KC_SCLN),S(KC_2),
-       _______,    S(3),       S(4),       S(KC_NUBS), S(KC_GRV), KC_GRV,        S(KC_EQL),  S(KC_5),    S(KC_COMM), KC_DOT,     KC_SCLN,   _______,
+       _______,    _______,    _______,    _______,    _______,   _______,       S(KC_1),    S(KC_COMM), S(KC_DOT),  KC_EQL,     S(KC_7),   _______,
+       _______,    _______,    _______,    _______,    _______,   _______,       S(KC_SLSH), S(KC_9),    S(KC_0),    KC_MINS,    S(KC_SCLN),S(KC_2),
+       _______,    _______,    _______,    _______,    _______,   _______,       S(KC_EQL),  S(KC_5),    S(KC_COMM), KC_DOT,     KC_SCLN,   _______,
                                 _______,    _______,                                                   _______,    _______,
 
                                      _______,       _______,      _______,       _______, TO(0),   _______,
@@ -74,10 +74,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,    _______,    _______,    _______,    _______,   _______,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
         _______,    _______,    _______,    _______,    _______,   _______,       KC_PSCR, KC_7,    KC_8,    KC_9,    KC_PPLS, KC_F12,
         _______,    _______,    _______,    _______,    _______,   _______,       KC_0,    KC_4,    KC_5,    KC_6,    KC_0,    _______,
-        QK_BOOT,    _______,    _______,    _______,    _______,   _______,       KC_0,    KC_1,    KC_2,    KC_3,    KC_DOT,  KC_COMM,
+        _______,    _______,    _______,    _______,    _______,   _______,       KC_0,    KC_1,    KC_2,    KC_3,    KC_DOT,  KC_COMM,
                                 _______,    _______,                                                _______,    _______,
 
                                          _______,    _______,      _______,       _______, TO(0),   _______,
-                                                     _______,      _______,       _______, TO(0)
+                                                     _______,      _______,       _______, _______
     )
 };
---
+++ keyboards/smarwei/small_manuform/keymaps/default/keymap_right.c
@@ -0,0 +1,73 @@
+#include QMK_KEYBOARD_H
+
+#define _LOWER 3
+#define _RAISE 4
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┬───┬───┬───┐
+     * │F7 │F8 │F9 │F10│F11|F12|
+     * ├───┼───┼───┼───┼───┼───┤
+     * │ 6 │ 7 │ 8 │ 9 │ 0 |TG |
+     * ├───┼───┼───┼───┼───┼───|
+     * │ J │ L │ U │ Y │ ; |F24|
+     * ├───┼───┼───┼───┼───┼───|
+     * │ M │ N │ E │ I │ O |CPS|
+     * ├───┼───┼───┼───┼───┼───|
+     * │ K │ H │ , │ . │ / |PRT|
+     * └───┴───┼───┼───┼───┴───┘
+     *         │ - │ = |
+     * ┌───┬───┼───┼───┘
+     * │RET│SPC│MO1│
+     * ├───┼───┼───┘
+     * │LUP│LDN│
+     * └───┴───┘
+     */
+    [0] = LAYOUT_std(
+        KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,
+        KC_6,    KC_7,    KC_8,    KC_9,     KC_0,     SH_TG,
+        KC_J,    KC_L,    KC_U,    KC_Y,     KC_SCLN,  KC_F24,
+        KC_M,    KC_N,    KC_E,    KC_I,     KC_O,     KC_CAPS,
+        KC_K,    KC_H,    KC_COMM, KC_DOT,   KC_SLASH, KC_RSFT,
+                          KC_MINS, KC_EQL,
+
+        KC_ENT,  MO(3),   KC_SPC,
+        KC_LGUI, KC_LSFT
+    ),
+
+    [1] = LAYOUT_std(
+        _______,    _______,    _______,    _______,    _______, _______,
+        _______,    _______,    _______,    _______,    _______, _______,
+        S(KC_1),    S(KC_COMM), S(KC_DOT),  KC_EQL,  S(KC_7),    _______,
+        S(KC_SLSH), S(KC_9),    S(KC_0),    KC_MINS, S(KC_SCLN), S(KC_2),
+        S(KC_EQL),  S(KC_5),    S(KC_COMM), KC_DOT,  KC_SCLN,    _______,
+                                _______,    _______,
+
+        _______,    _______,    _______,
+        _______,    _______
+    ),
+
+    [2] = LAYOUT_std(
+        _______, _______, _______, _______, _______, _______,
+        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+        KC_PSCR, KC_7,    KC_8,    KC_9,    KC_PPLS, KC_F12,
+        KC_0,    KC_4,    KC_5,    KC_6,    KC_0,    _______,
+        KC_0,    KC_1,    KC_2,    KC_3,    KC_DOT,  KC_COMM,
+                          KC_0,    KC_0,
+
+        _______, _______, _______,
+        _______, _______
+    ),
+
+    [3] = LAYOUT_std(
+        _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,
+        KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_DEL,  KC_ESC,
+        KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC, KC_MENU,
+        KC_BTN1, KC_BTN2, KC_BTN3, KC_MS_L, KC_MS_R, KC_WH_R,
+                          KC_0,    KC_0,
+
+        _______, _______, _______,
+        _______, _______
+    )
+};
