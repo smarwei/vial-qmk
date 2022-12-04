9db01ea48ede62c27fdb05d0f65bf2c99708e332
Arne Weiss <arne.weiss@udo.edu>
2022-12-04 21:04:17 +0100

    finished Dactyl Miniform

--- keyboards/smarwei/new_mini_manuform/keymaps/vial/keymap.c
+++ keyboards/smarwei/new_mini_manuform/keymaps/vial/keymap.c
@@ -32,9 +32,9 @@ bool caps_word_press_user(uint16_t keycode) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT_std(
-        KC_TAB,  KC_Q,    KC_W,     KC_F,    KC_P,     KC_B,        KC_J,    KC_L,    KC_U,    KC_Y,     KC_SCLN,  KC_F24,
-        KC_ESC,  KC_A,    KC_R,     KC_S,    KC_T,     KC_G,        KC_M,    KC_N,    KC_E,    KC_I,     KC_O,     KC_CAPS,
-        CAPSWRD, KC_Z,    KC_X,     KC_C,    KC_D,     KC_V,        KC_K,    KC_H,    KC_COMM, KC_DOT,   KC_SLASH, KC_RSFT,
+        RESET,   KC_Q,    KC_W,     KC_F,    KC_P,     KC_B,        KC_J,    KC_L,    KC_U,    KC_Y,     KC_SCLN,  KC_F24,
+        RESET,   KC_A,    KC_R,     KC_S,    KC_T,     KC_G,        KC_M,    KC_N,    KC_E,    KC_I,     KC_O,     KC_CAPS,
+        RESET,   KC_Z,    KC_X,     KC_C,    KC_D,     KC_V,        KC_K,    KC_H,    KC_COMM, KC_DOT,   KC_SLASH, KC_RSFT,
                           KC_LGUI,  KC_LALT,                                          KC_MINS, KC_EQL,
 
                                            MO(1),    KC_LSFT,       KC_SPACE,TD(0),
